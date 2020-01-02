40246a71fa35d22825c7ae8d66c8280e8479abbf
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-01-02 17:07:15 +0700

    Disabled finish test button before answering every question

--- js/test-soal.js
+++ js/test-soal.js
@@ -11,6 +11,10 @@ $(document).ready(function () {
   blockRightClick();
   reloadOnWindowBlur();
 
+  $('input').on('click', function() {
+    countAnsQuestion();
+  });
+
   if ($(window).width() <= 970) {
 
     $(".question-choice-list").each(function(idChoice) {
@@ -37,6 +41,8 @@ $(document).ready(function () {
 function testSoalFullscreen() {
   openFullscreen(document.documentElement);
   $('#confirmationModal').modal('hide');
+  ujianTimerCountdown(60);
+  countAnsQuestion();
 }
 
 function openFullscreen(elem) {
@@ -49,4 +55,76 @@ function openFullscreen(elem) {
   } else if (elem.msRequestFullscreen) { /* IE/Edge */
     elem.msRequestFullscreen();
   }
+}
+
+function countAnsQuestion() {
+  var totalAns = 0;
+  var totalQuestion = 0;
+
+  var known_question_name = [];
+
+  $("input").each(function(idInput) {
+    if ($(this)[0].checked == true) {
+      totalAns += 1;
+    }
+
+    if (!known_question_name.includes($(this).attr("name"))) {
+      known_question_name.push($(this).attr("name"));
+      totalQuestion += 1;
+    }
+  });
+
+  $("#answerCount")[0].innerHTML = totalAns + " / " + totalQuestion;
+
+  if(totalAns == totalQuestion) {
+    $("a[data-target='#finishedAnswer']")[0].classList.remove('disabled');
+  }
+}
+
+function ujianTimerCountdown(m, s=0) {
+  timer(m, s);
+
+  setInterval(() => {
+    if (!Timer) {
+      var currentTime = $("#globalTimer")[0].innerHTML.split(":");
+      var currentMin = parseInt(currentTime[0]);
+      var currentSec = parseInt(currentTime[1]);
+
+      if (currentMin > 0 || currentSec > 0) {
+        timer(currentMin, currentSec);
+      } else {
+        autoSubmit();
+      }
+      console.log("timer is off");
+    }
+  }, 1000);
+}
+
+function timer(m, s=0) {
+  var menit = s == 0 ? m - 1 : m;
+  var detik = s == 0 ? 59 : s;
+
+  Timer = setInterval(() => {
+    if (menit === 0 && detik === 0) {
+      clearInterval(Timer);
+      Timer = null;
+      autoSubmit();
+      return;
+    }
+    menit -= detik === 0 ? 1 : 0;
+    detik = mod(detik - 1, 60);
+
+    // console.log(menit + ":" + detik);
+    $("#globalTimer")[0].innerHTML = (menit.toString().length == 1 ? "0" : "") + menit + ":" +
+                                      (detik.toString().length == 1 ? "0" : "") + detik;
+  }, 1000);
+}
+
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
+
+function autoSubmit() {
+  console.log("submit");
+  $("#testAnswer").submit();
 }
